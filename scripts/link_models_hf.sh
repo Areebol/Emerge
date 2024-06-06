ae8e55716ed2c261b5787440f8897cb3e83e66d3
Areebol <103118945+Areebol@users.noreply.github.com>
2024-06-06 09:35:34 +0800

    Added modles hf script

---
+++ scripts/link_models_hf.sh
@@ -0,0 +1,5 @@
+# bash 
+LIB_HOME=/share/home/zhangshuhai/miniconda3/lib/python3.12
+
+cp ./models/modeling_llama.py $LIB_HOME/site-packages/transformers/models/llama
+cp ./models/modeling_qwen.py $LIB_HOME/site-packages/transformers/models/qwen2
